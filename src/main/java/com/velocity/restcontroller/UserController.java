c4ea5b9d6599404f159d35d738152d1c4671405b
ParagGautam001 <paraggautam123@gmail.com>
2026-04-01 16:42:09 +0530

    Update UserController.java

--- src/main/java/com/velocity/restcontroller/UserController.java
+++ src/main/java/com/velocity/restcontroller/UserController.java
@@ -1,14 +1,7 @@
-package com.velocity.restcontroller;
+package com.velocity.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.velocity.dto.ResponseDto;
-import com.velocity.service.UserService;
-import com.velocity.user.User;
 
 @RestController
 public class UserController {
